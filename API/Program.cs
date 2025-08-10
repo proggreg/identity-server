0673770ebe92ea5acd9e41c9379244f761655294
Greg Field <greg.field1992@gmail.com>
2025-08-10 12:05:02 +0100

    setting up API

---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options => {
+        options.Authority = "https://localhost:5001";
+        options.TokenValidationParameters.ValidateAudience = false;
+    });
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/identity", (ClaimsPrincipal user) => 
+        user.Claims.Select(c => new {c.Type, c.Value}))
+            .RequireAuthorization();
+
+app.Run();
