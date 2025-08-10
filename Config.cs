4bd8bc477e09e236581ae1fb45cb650894ad828b
Greg Field <greg.field1992@gmail.com>
2025-08-10 11:45:39 +0100

    add mcp clientcred

---
+++ Config.cs
@@ -0,0 +1,32 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace IdentityServerTutorial;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new ApiScope[]
+        {
+            new ApiScope(name: "mcp", displayName: "MCP"),
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+            {
+                ClientId = "mcp",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("secret".Sha256()) },  
+                AllowedScopes = { "mcp" }
+                
+            }
+            
+        };
+}
