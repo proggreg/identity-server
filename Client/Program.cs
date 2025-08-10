a32fae741d3fb201d3ffa30cdc579262a406d60c
Greg Field <greg.field1992@gmail.com>
2025-08-10 12:14:34 +0100

    add client

---
+++ Client/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using Duende.IdentityModel.Client;
+
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+    return;
+}
+
+var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+{
+    Address = disco.TokenEndpoint,
+    ClientId = "client",
+    ClientSecret = "secret",
+    Scope = "mcp"
+
+});
+
+if (tokenResponse.IsError)
+{
+    Console.WriteLine(tokenResponse.Error);
+    Console.WriteLine(tokenResponse.ErrorDescription);
+    return;
+}
+
+Console.WriteLine(tokenResponse.AccessToken);
+
+var apiClient = new HttpClient();
+apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+var response = await apiClient.GetAsync("https://localhost:5001/api/identity");
+
+if (response.IsSuccessStatusCode)
+{
+    Console.WriteLine(await response.Content.ReadAsStringAsync());
+}
+else
+{
+    var doc = JsonDocument.Parse(await response.Content.ReadAsStringAsync()).RootElement;
+    Console.WriteLine(JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true }));
+}
